07c475eee0b36e9944dd857dd837a78f94bbe625
ddsol <github@digitaldesksolutions.com>
2015-04-19 17:34:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# mp3-duration ![Travis-Build](https://travis-ci.org/ddsol/mp3-duration.svg?branch=master)
+
+> Calculate duration of an MP3
+
+
+## Install
+
+```
+$ npm install --save mp3-duration
+```
+
+## Usage
+
+```javascript
+var mp3Duration = require('mp3-duration');
+
+mp3Duration('file.mp3', function (err, duration) {
+	if (err) return console.log(err.message);
+	console.log('Your file is '+duration+' seconds long');
+});
+
+```
+
+## API
+
+## mp3Duration(filePath, callback) 
+
+### filePath
+
+Type: `string`
+
+Path to the file
+
+### callback(error, duration)
+
+Type: `function`
+
+Callback to be called once duration is calculated
+
+## License
+
+MIT © Han de Boer
---
+++ tests/test.js
@@ -0,0 +1,20 @@
+var assert = require('assert'),
+    mp3Duration=require('../index.js');
+
+describe('mp3Duration', function(){
+    it('returns a correct value for VBR duration', function (done) {
+        mp3Duration('./tests/demo - vbr.mp3', function (err, length) {
+            if (err) console.log(err.stack);
+            assert.equal(err, null, (err||{}).message);
+            assert.equal(length, 285.727, 'Length not as expected');
+            done();
+        });
+    });
+    it('returns a correct value for CBR duration', function (done) {
+        mp3Duration('./tests/demo - cbr.mp3', true, function (err, length) {
+            assert.equal(err, null, (err||{}).message);
+            assert.equal(length, 285.675, 'Length not as expected');
+            done();
+        });
+    });
+});
